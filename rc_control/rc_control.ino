6c8e70a5f6d772fc2bcffe76d9558b3eb932a544
Yannis Chatzikonstantinou <yconst@gmail.com>
2021-11-20 23:54:47 +0200

    use pulseposition library

--- rc_control/rc_control.ino
+++ rc_control/rc_control.ino
@@ -1,34 +1,40 @@
-#include <InterpolationLib.h>
-#include <PPMReader.h>
+#include <PulsePosition.h>
+#include <PulsePositionIMXRT.h>
 
-// Initialize a PPMReader on digital pin 3 with 6 expected channels.
-byte interruptPin = 3;
-byte channelAmount = 6;
-PPMReader ppm(interruptPin, channelAmount);
+#include <InterpolationLib.h>
+
+byte rxPin = 5;
+PulsePositionInput myInput(FALLING);
 
 // Setup the interpolator
 const int numValues = 2;
 double xValues[10] = {   1000,  2000 }; // RC signal range
-double yValues[10] = { -50000, 50000 }; // Motor desired velocity range
+double yValues[10] = { -100000, 100000 }; // Motor desired velocity range
+//double yValues[10] = { -300000, 300000 }; // More aggressive limits
 
 void setup() {
-    Serial.begin(115200);
+  myInput.begin(rxPin);
+    //                                                                                                                                                                                                                     Serial.begin(115200);
+    Serial2.begin(115200);
     delay(100);
     // Immediately set Tinymovr to closed loop control state
-    Serial.print(".A\n");
+    //Serial.print(".A\n");
+    Serial2.print(".A\n");
+    delay(10);
 }
 
 void loop() {
     // Read value from channel (0 in this case)
-    uint8_t channel = 0;
-    double xValue = (double)ppm.latestValidChannelValue(channel, 0);
+    uint8_t channel = 3;
+    float xValue = myInput.read(channel);
 
     // If value is within range, print it out to serial as a command
     if (xValue >= 1000 && xValue <= 2000) {
         int32_t vel_out = (int32_t)Interpolation::Linear(xValues, yValues, numValues, xValue, false);
-        Serial.printf(".V%d\n", vel_out);
+        //Serial.printf(".V%d\n", vel_out);
+        Serial2.printf(".V%d\n", vel_out);
     }
 
-    // 200Hz = 5ms delay
-    delay(5);
+    // 100Hz = 10ms delay
+    delay(10);
 }
